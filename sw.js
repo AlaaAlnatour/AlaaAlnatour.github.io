e5a721ff7bd844deec608fe8c89273b8f752f9c8
alaa <prince.alaa9@gmail.com>
2017-01-08 23:32:59 +0200

    second commit

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ self.addEventListener('push', function(event) {
     const title = 'Push Codelab';
     const options = {
         body: `${event.data.text()}`,
-        icon: 'images/icon.png',
+        icon: 'images/1.png',
         badge: 'images/badge.png'
     };
 
